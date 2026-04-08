8acaf1a46a28593983c31688924bd31d2fff18b6
Andrea Treccani <treccamostro26@gmail.com>
2026-04-08 15:38:34 +0200

    fix build

--- portfolio-andrea/src/components/Contact.jsx
+++ portfolio-andrea/src/components/Contact.jsx
@@ -1,21 +1,37 @@
-<div className="contact-links">
-  <a href="mailto:treccaniandrea6@gmail.com">
-    treccaniandrea6@gmail.com
-  </a>
+function Contact() {
+  return (
+    <section id="contact" className="section">
+      <div className="container">
+        <h3 className="section-title">Contact</h3>
 
-  <a
-    href="https://github.com/treccaniandrea6-prog"
-    target="_blank"
-    rel="noreferrer"
-  >
-    GitHub
-  </a>
+        <p className="section-text">
+          Interested in my work or want to connect? You can find me here:
+        </p>
 
-  <a
-    href="https://www.linkedin.com/in/andrea-treccani-6346b01a3/"
-    target="_blank"
-    rel="noreferrer"
-  >
-    LinkedIn
-  </a>
-</div>
+        <div className="contact-links">
+          <a href="mailto:treccaniandrea6@gmail.com">
+            treccaniandrea6@gmail.com
+          </a>
+
+          <a
+            href="https://github.com/treccaniandrea6-prog"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/andrea-treccani-6346b01a3/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
